7fd55ba944ed89a34499a52bab1fcaa352a9fe93
westmarquest <9certified@gmail.com>
2024-06-04 22:43:55 -0700

    bcrypt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+);
+// mongoose.connect(
+//   "mongodb+srv://9certified:3c0GVnzwxOZ2AiND@cluster0.urkpnwy.mongodb.net/bookdb?retryWrites=true&w=majority&appName=Cluster0"
+// );
 module.exports = mongoose.connection;
